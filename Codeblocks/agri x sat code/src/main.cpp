a802f9bcaac1377bfb3733d49cfd84d42db37258
franfreezy <frandelwanjawa@yahoo.com>
2024-11-10 15:08:51 +0300

    Normal downlinking of data

--- Codeblocks/agri x sat code/src/main.cpp
+++ Codeblocks/agri x sat code/src/main.cpp
@@ -2,24 +2,38 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial downlink(12,13);
+SoftwareSerial OBCComms(18,19);
 int uplinkready =5;
 int downlinkready =2;
 int commandstatus =4;
-
+int indicator1=0;
 
 
 void setup() {
   Serial.begin(9600);
-  downlink.begin(9600);
-  downlink.println();
+  //downlink.begin(9600);
+  //downlink.println();
   pinMode(uplinkready,INPUT_PULLUP);
-  pinMode(downlinkready,INPUT_PULLUP);
+  pinMode(downlinkready,OUTPUT);
   pinMode(commandstatus,OUTPUT);
 }
 
 
 void loop() {
-  
+  if(millis()>=2000){
+    indicator1=millis();
+    digitalWrite(downlinkready,HIGH);
+    delay(3000);
+    OBCComms.begin(9600);
+    String message=OBCComms.readStringUntil('~');
+    OBCComms.end();
+    delay(50);
+    downlink.begin(9600);
+    downlink.println(message);
+    downlink.end();
+
+  }
 
 }
+
 
