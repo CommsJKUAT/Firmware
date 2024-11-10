92fcc4ee2c813f27493bfa5f9a53fc015c8f784f
franfreezy <frandelwanjawa@yahoo.com>
2024-11-10 15:37:55 +0300

    transmitter configured

--- Codeblocks/agri x sat code/src/main.cpp
+++ Codeblocks/agri x sat code/src/main.cpp
@@ -1,61 +1,66 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial downlink(12,13);
-SoftwareSerial OBCComms(18,19);
-int commandready =5;
-int downlinkready =2;
-int commandstatus =4;
-int indicator1=0;
-int indicator2=0;
+SoftwareSerial downlink(12, 13);
+SoftwareSerial OBCComms(18, 19);
+int commandready = 5;
+int downlinkready = 2;
+int commandstatus = 4;
+int indicator1 = 0;
+int indicator2 = 0;
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  //downlink.begin(9600);
-  //downlink.println();
-  pinMode(commandready,INPUT_PULLUP);
-  pinMode(downlinkready,OUTPUT);
-  pinMode(commandstatus,OUTPUT);
+  // downlink.begin(9600);
+  // downlink.println();
+  pinMode(commandready, INPUT_PULLUP);
+  pinMode(downlinkready, OUTPUT);
+  pinMode(commandstatus, OUTPUT);
 }
 
-
-void loop() {
-  if(millis()>=20000){
-    indicator1=millis();
-    digitalWrite(downlinkready,HIGH);
+void loop()
+{
+  if (millis() >= 20000)
+  {
+    indicator1 = millis();
+    digitalWrite(downlinkready, HIGH);
     delay(3000);
     OBCComms.begin(9600);
-    String message=OBCComms.readStringUntil('~');
+    String message = OBCComms.readStringUntil('~');
     OBCComms.end();
     delay(50);
     downlink.begin(9600);
     downlink.println(message);
     downlink.end();
   }
-  if(millis()>=1000){
-    indicator2=millis();
-    digitalWrite(commandstatus,HIGH);
-    OBCComms.begin(9600);
-    delay(2000);
-    OBCComms.println("command~");
-    OBCComms.end();
-    int status=digitalRead(commandready);
-    if(status==1){
-      downlink.begin(9600);
-      delay(50);
-      String command= downlink.readStringUntil('~');
+  if (millis() >= 1000)
+  {
+    indicator2 = millis();
+    downlink.begin(9600);
+    delay(50);
+    String command = downlink.readStringUntil('~');
+    if (command == "command")
+    {
+      digitalWrite(commandstatus, HIGH);
+
       downlink.end();
       OBCComms.begin(9600);
-      delay(20);
-      OBCComms.println(command);
+      delay(2000);
+      OBCComms.println("command~");
       OBCComms.end();
+      int status = digitalRead(commandready);
+      if (status == 1)
+      {
+        downlink.begin(9600);
+        delay(50);
+        String command = downlink.readStringUntil('~');
+        downlink.end();
+        OBCComms.begin(9600);
+        delay(20);
+        OBCComms.println(command);
+        OBCComms.end();
+      }
     }
-
-
-
   }
-
 }
-
-
