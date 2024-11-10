aaeb973f773528bcb12ccad9b72837f5e6c3c3bb
franfreezy <frandelwanjawa@yahoo.com>
2024-11-10 15:25:34 +0300

    Lets see if this works

--- Codeblocks/agri x sat code/src/main.cpp
+++ Codeblocks/agri x sat code/src/main.cpp
@@ -3,24 +3,25 @@
 
 SoftwareSerial downlink(12,13);
 SoftwareSerial OBCComms(18,19);
-int uplinkready =5;
+int commandready =5;
 int downlinkready =2;
 int commandstatus =4;
 int indicator1=0;
+int indicator2=0;
 
 
 void setup() {
   Serial.begin(9600);
   //downlink.begin(9600);
   //downlink.println();
-  pinMode(uplinkready,INPUT_PULLUP);
+  pinMode(commandready,INPUT_PULLUP);
   pinMode(downlinkready,OUTPUT);
   pinMode(commandstatus,OUTPUT);
 }
 
 
 void loop() {
-  if(millis()>=2000){
+  if(millis()>=20000){
     indicator1=millis();
     digitalWrite(downlinkready,HIGH);
     delay(3000);
@@ -31,6 +32,27 @@ void loop() {
     downlink.begin(9600);
     downlink.println(message);
     downlink.end();
+  }
+  if(millis()>=1000){
+    indicator2=millis();
+    digitalWrite(commandstatus,HIGH);
+    OBCComms.begin(9600);
+    delay(2000);
+    OBCComms.println("command~");
+    OBCComms.end();
+    int status=digitalRead(commandready);
+    if(status==1){
+      downlink.begin(9600);
+      delay(50);
+      String command= downlink.readStringUntil('~');
+      downlink.end();
+      OBCComms.begin(9600);
+      delay(20);
+      OBCComms.println(command);
+      OBCComms.end();
+    }
+
+
 
   }
 
