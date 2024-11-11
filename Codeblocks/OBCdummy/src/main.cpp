907772ce688439dffd1a98d92aeddc075fb3e91a
franfreezy <frandelwanjawa@yahoo.com>
2024-11-11 13:32:58 +0300

    OBC comms interfacing version1

---
+++ Codeblocks/OBCdummy/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+int status1 = 5;
+int status2 =18;
+void setup() {
+  pinMode(status1,OUTPUT);
+  pinMode(status2,OUTPUT);
+}
+
+
+void modeNormal(){
+  digitalWrite(status1,LOW);
+  digitalWrite(status2,LOW);
+}
+void modeidle(){
+  digitalWrite(status1,LOW);
+  digitalWrite(status2,LOW);
+}
+void modeSafe(){
+  digitalWrite(status1,LOW);
+  digitalWrite(status2,LOW);
+}
+void modeEmergency(){
+  digitalWrite(status1,HIGH);
+  digitalWrite(status2,HIGH);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
