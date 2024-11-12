d0d39a8f3b82fcd1742761c1365b85b35afb87df
franfreezy <frandelwanjawa@yahoo.com>
2024-11-12 19:19:50 +0300

    Comms onboard

--- Codeblocks/agri x sat code/src/main.cpp
+++ Codeblocks/agri x sat code/src/main.cpp
@@ -1,27 +1,26 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <HardwareSerial.h>
+
 #define comms_rx 5
-#define comms_tx  21
+#define comms_tx 21
 
 SoftwareSerial downlink(12, 13);  // RX, TX for downlink
-HardwareSerial OBCComms(1);  // RX, TX for OBCComms
+HardwareSerial OBCComms(1);       // RX, TX for OBCComms
 
 const int commandready = 19;  // Input from OBC
 const int transmitready = 18; // Input from OBC
 const int downlinkready = 2;
 const int commandstatus = 4;  // Output to interrupt
 
-int sat1 = 1;
-int sat2 = 1;
-
 bool normal = false;
 bool initialisation = false;
 bool critical = false;
 
 void setup() {
-  Serial.begin(9600);
-  downlink.begin(9600);
+  Serial.begin(115200);            // Debugging port
+  downlink.begin(115200);            // Downlink communication
+  OBCComms.begin(115200, SERIAL_8N1, 5, 21);  // UART setup for OBCComms
 
   pinMode(commandready, INPUT_PULLUP);
   pinMode(transmitready, INPUT_PULLUP);
@@ -36,82 +35,41 @@ void loop() {
   static unsigned long lastTransmissionCheck = 0;
   static unsigned long lastCommandSignal = 0;
 
-  // Check every 1000 ms if we should update `sat1` and `sat2`
+  int sat1 = digitalRead(commandready);
+  int sat2 = digitalRead(transmitready);
+
+ 
   if (millis() - lastTransmissionCheck >= 1000) {
-    sat1 = digitalRead(commandready);
-    sat2 = digitalRead(transmitready);
+    
     Serial.println(sat1);
     Serial.println(sat2);
-    lastTransmissionCheck = millis();  // Update the timestamp
+    lastTransmissionCheck = millis();  
   }
-
-  // Initialization mode
-  if (sat1 == 0 && sat2 == 0) {
-    downlink.println("INM~");
-    initialisation = true;
-    while (initialisation) {
-      String message = Serial.readStringUntil('~');
-      downlink.println("INM~");  // Repeat mode indicator
-
-      // Read the updated state directly from `commandready` and `transmitready`
-      int in1 = digitalRead(commandready);
-      int in2 = digitalRead(transmitready);
-      if (in1 != 0 || in2 != 0) {
-        initialisation = false;  // Exit initialization mode
-      }
-    }
-  }
-
-  // Normal mode
-  if (sat1 == 0 && sat2 == 1) {
+  if (sat1==1 and sat2==1)
+  {
     downlink.println("NM~");
-    normal = true;
-    while (normal) {
-      String message = Serial.readStringUntil('~');
-      downlink.println(message);  // Send received message
-      delay(3000);  // Delay for 3 seconds between transmissions
-
-      // Check if we should exit the normal mode
-      int nm1 = digitalRead(commandready);
-      int nm2 = digitalRead(transmitready);
-      Serial.println(nm1);
-      Serial.println(nm2);
-      String meso=OBCComms.readStringUntil('}');
-      Serial.println(meso);
-      if (nm1 != 0 || nm2 != 1) {
-        normal = false;  // Exit normal mode
-      }
+  }
+  if (sat1==1 and sat2==0)
+  {
+    while (sat2==0)
+    {
+      String dataOBC=OBCComms.readStringUntil('}');
+      sat2=digitalRead(transmitready);
+    }
+    
+  }
+  if (sat1==0 and sat2==1)
+  {
+    downlink.println("CM~");
+  }
+  if (sat1==0 and sat2==0)
+  {
+    while (sat2==0)
+    {
+      String dataOBC=OBCComms.readStringUntil('}');
+      sat2=digitalRead(transmitready);
     }
   }
 
-  // Critical mode
-  if (sat1 == 1 && sat2 == 0) {
-    downlink.println("CTM~");
-    critical = true;
-    while (critical) {
-      String message = Serial.readStringUntil('~');
-
-      // Check if we should exit the critical mode
-      int ct1 = digitalRead(commandready);
-      int ct2 = digitalRead(transmitready);
-      if (ct1 != 1 || ct2 != 0) {
-        critical = false;  // Exit critical mode
-      }
-    }
-  }
-
-  // Command signal check every 5000 ms
-  if (millis() - lastCommandSignal >= 5000) {
-    lastCommandSignal = millis();  // Update the timestamp
-
-    while (downlink.available()) {
-      String message = downlink.readStringUntil('~');
-
-      if (message.startsWith("Cm")) {
-        digitalWrite(commandstatus, HIGH);  // Trigger command status
-        message.remove(0, 3);               // Remove "Cm" prefix
-        Serial.println(message);            // Print the command
-      }
-    }
-  }
+  
 }
